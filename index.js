3567ed817a33d2b68e41d461333aff56d98990a9
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-07-04 13:14:29 +0300

    Summary

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+// console.log('Start work')
+
+// setTimeout(() => {
+//     console.log('Start work 5000 sec!!!!')
+// }, 5000);
+
+// console.log('WORK!!!!!!!!!!!!!!!!! DEV RUN')
+
+// setTimeout(() => {
+//     console.log('Start run dev work 10000 sec!!!!')
+// }, 10000);
